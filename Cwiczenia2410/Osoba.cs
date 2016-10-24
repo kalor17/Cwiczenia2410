eda1ada6698f4dc9f7900eb8234fc6d80004ff58
kalor17 <wisniak17@gmail.com>
2016-10-24 14:47:32 +0200

    Cwiczenia2410

---
+++ Cwiczenia2410/Osoba.cs
@@ -0,0 +1,29 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Cwiczenia2410
+{
+    class Osoba
+    {
+        public string imie;
+        public string nazwisko;
+        public int rok;
+
+        public Osoba(string imie, string nazwisko, int rok)
+        {
+            this.imie = imie;
+            this.nazwisko = nazwisko;
+            this.rok = rok;
+        }
+
+        public void WypiszInfo()
+        {
+            Console.WriteLine(imie);
+            Console.WriteLine(nazwisko);
+            Console.WriteLine(rok);
+        }
+    }
+}
---
+++ Cwiczenia2410/Program.cs
@@ -0,0 +1,18 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Cwiczenia2410
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            Osoba lukasz = new Osoba("Lukasz", "Zakrzewski", 1994);
+            lukasz.WypiszInfo();
+            Console.ReadKey();
+        }
+    }
+}
